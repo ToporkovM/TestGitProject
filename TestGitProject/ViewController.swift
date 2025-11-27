ef06a50be2312cac62976c185bd9bf979e188c81
max <toporkovmaksim7@gmail.com>
2025-11-27 21:59:24 +0700

    Initial Commit

---
+++ TestGitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGitProject
+//
+//  Created by Максим on 27.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
